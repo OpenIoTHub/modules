5899c94ed3561b24fffa74a04a06b50c5379de50
Farry <newfarry@126.com>
2020-05-31 22:23:10 +0800

    update：发现网关重复

--- lib/api/OpenIoTHub/SessionApi.dart
+++ lib/api/OpenIoTHub/SessionApi.dart
@@ -2,8 +2,27 @@ import 'package:modules/api/OpenIoTHub/OpenIoTHubChannel.dart';
 import 'package:openiothub_grpc_api/pb/service.pb.dart';
 import 'package:openiothub_grpc_api/pb/service.pbgrpc.dart';
 import 'package:grpc/grpc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class SessionApi {
+  static Future<void> saveSessions() async {
+    SessionList sessionList = await getAllSession();
+    List<String> scl = sessionList.sessionConfigs.map((SessionConfig sc) {
+      return jsonEncode(sc);
+    });
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('sessions', scl);
+    List<String> newscl = await prefs.getStringList('sessions');
+    print("newscl:$newscl");
+  }
+
+  static Future<void> loadSessions() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> newscl = await prefs.getStringList('sessions');
+    print("newscl:$newscl");
+  }
+
 //  TODO 可以选择grpc所执行的主机，可以是安卓本机也可以是pc，也可以是服务器
   static Future createOneSession(SessionConfig config) async {
     final channel = await Channel.getOpenIoTHubChannel();
@@ -11,6 +30,7 @@ class SessionApi {
     final response = await stub.createOneSession(config);
     print('Greeter client received: ${response}');
     channel.shutdown();
+    saveSessions();
   }
 
   static Future deleteOneSession(SessionConfig config) async {
@@ -18,6 +38,7 @@ class SessionApi {
     final stub = SessionManagerClient(channel);
     await stub.deleteOneSession(config);
     channel.shutdown();
+    saveSessions();
   }
 
   static Future<SessionList> getAllSession() async {
@@ -44,6 +65,7 @@ class SessionApi {
     final response = await stub.refreshmDNSProxyList(sessionConfig);
     print('Greeter client received: ${response}');
     channel.shutdown();
+    saveSessions();
     return response;
   }
 }
