dd135e5dfd795e2db85126a579656063c25a378e
iotserv <yu@iotserv.com>
2020-07-19 13:57:09 +0800

    update:添加到服务器grpc操作的jwt验证

--- lib/api/Server/HttpManager.dart
+++ lib/api/Server/HttpManager.dart
@@ -1,6 +1,7 @@
 import 'package:modules/api/OpenIoTHub/SessionApi.dart';
 import 'package:modules/api/OpenIoTHub/Utils.dart';
 import 'package:openiothub_grpc_api/pb/service.pb.dart' as openiothub;
+import 'package:openiothub_grpc_api/pb/service.pb.dart';
 import 'package:server_grpc_api/pb/service.pb.dart';
 import 'package:server_grpc_api/pb/service.pbgrpc.dart' as server;
 import 'package:grpc/grpc.dart';
@@ -10,7 +11,9 @@ class HttpManager {
 //  rpc CreateOneHTTP (HTTPConfig) returns (HTTPConfig) {}
   static Future<HTTPConfig> CreateOneHTTP(HTTPConfig httpConfig) async {
     final channel = await Channel.getServerChannel(httpConfig.runId);
-    final stub = server.HttpManagerClient(channel);
+    SessionConfig sessionConfig = await SessionApi.getOneSession(httpConfig.runId);
+    final stub = server.HttpManagerClient(channel, options: CallOptions(metadata: {'jwt': sessionConfig.token}));
+
     HTTPConfig httpConfigResponse = await stub.createOneHTTP(httpConfig);
     print('httpConfigResponse: ${httpConfigResponse}');
     channel.shutdown();
@@ -19,14 +22,18 @@ class HttpManager {
 //  rpc DeleteOneHTTP (HTTPConfig) returns (Empty) {}
   static Future<void> DeleteOneHTTP(HTTPConfig httpConfig) async {
     final channel = await Channel.getServerChannel(httpConfig.runId);
-    final stub = server.HttpManagerClient(channel);
+    SessionConfig sessionConfig = await SessionApi.getOneSession(httpConfig.runId);
+    final stub = server.HttpManagerClient(channel, options: CallOptions(metadata: {'jwt': sessionConfig.token}));
+
     await stub.deleteOneHTTP(httpConfig);
     channel.shutdown();
   }
 //  rpc GetOneHTTP (HTTPConfig) returns (HTTPConfig) {}
   static Future<HTTPConfig> GetOneHTTP(HTTPConfig httpConfig) async {
     final channel = await Channel.getServerChannel(httpConfig.runId);
-    final stub = server.HttpManagerClient(channel);
+    SessionConfig sessionConfig = await SessionApi.getOneSession(httpConfig.runId);
+    final stub = server.HttpManagerClient(channel, options: CallOptions(metadata: {'jwt': sessionConfig.token}));
+
     HTTPConfig newHttpConfig = await stub.getOneHTTP(httpConfig);
     channel.shutdown();
     return newHttpConfig;
@@ -34,7 +41,9 @@ class HttpManager {
 //  rpc GetAllHTTP (Empty) returns (HTTPList) {}
   static Future<HTTPList> GetAllHTTP(openiothub.Device device) async {
     final channel = await Channel.getServerChannel(device.runId);
-    final stub = server.HttpManagerClient(channel);
+    SessionConfig sessionConfig = await SessionApi.getOneSession(device.runId);
+    final stub = server.HttpManagerClient(channel, options: CallOptions(metadata: {'jwt': sessionConfig.token}));
+
     server.Device sdevice = server.Device();
     sdevice.runId = device.runId;
     sdevice.addr = device.addr;
