29c495d8652d6732c82efcd12da996cdcbde4c6a
iotserv <yu@iotserv.com>
2020-10-20 01:00:19 +0800

    update

--- lib/pages/mdnsService/mdnsType2ModelMap.dart
+++ lib/pages/mdnsService/mdnsType2ModelMap.dart
@@ -5,7 +5,7 @@ import './components.dart';
 //兼容的mdns类型，最终的落地点都在ModelsMap里
 //没有的id和mac置空
 class MDNS2ModelsMap {
-  final Map<String, String> baseInfo = {
+  static final Map<String, String> baseInfo = {
     "name": "OpenIoTHub Entity",
     "model": WebPage.modelName,
     "mac": "",
@@ -16,11 +16,28 @@ class MDNS2ModelsMap {
     "firmware-respository": "https://github.com/iotdevice/*",
     "firmware-version": "version",
   };
+  static final PortService basebasePortService = PortService(
+      isLocal: true,
+      info: {
+        "name": "未知类型",
+        "model": "",
+        "mac": "",
+        "id": "",
+        "author": "Farry",
+        "email": "newfarry@126.com",
+        "home-page": "https://github.com/OpenIoTHub",
+        "firmware-respository": "https://github.com/OpenIoTHub",
+        "firmware-version": "version",
+      },
+      ip: "127.0.0.1",
+      port: 80);
   //从ios14开始mdns发现的类型需要在Info.plist注册才可以被发现
   //	https://github.com/OpenIoTHub/OpenIoTHub/blob/41b1869951691a9084c66501b3d267daa4216577/ios/Runner/Info.plist#L38
   //请新模型的开发者同步在上述OpenIoTHub中的Info.plist添加新的mdns类型，如：_http._tcp
   //TODO：后续将会自动添加到Info.plist
-  static Map<String, PortService> modelsMap = Map.from({
+      static
+
+  Map<String, PortService> modelsMap = Map.from({
     //    OpenIoTHub网关模型
     Config.mdnsGatewayService: PortService(
         isLocal: true,
@@ -81,7 +98,7 @@ class MDNS2ModelsMap {
           "email": "newfarry@126.com",
           "home-page": "https://www.home-assistant.io",
           "firmware-respository":
-              "https://github.com/home-assistant/home-assistant",
+          "https://github.com/home-assistant/home-assistant",
           "firmware-version": "version",
         },
         ip: "127.0.0.1",
